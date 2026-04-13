c57046daa4faf6111a419f62120e820b85e5f37c
gebeOCC <occ.gebe.barry@gmail.com>
2026-04-13 11:29:34 +0800

    From the start

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,9 @@
-import { router, useFocusEffect } from 'expo-router';
-import { useCallback, useState } from 'react';
 import { Text, View } from 'react-native';
 
 export default function Index() {
-    const [mount, setMount] = useState(false);
-
-    useFocusEffect(
-        useCallback(() => {
-            if (mount) {
-                router.navigate('/form');
-            }
-            setMount(true);
-        }, [mount])
-    )
-
     return (
         <View className='flex-1 items-center justify-center'>
-            <Text>Redirecting</Text>
+            <Text>Index</Text>
         </View>
     )
 }
